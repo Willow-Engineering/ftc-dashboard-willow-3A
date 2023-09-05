1f77abbc197ce126428c455d8387767c0cbd05cd
Astricklin3 <104530954+henryhull@users.noreply.github.com>
2023-09-05 10:43:38 -0500

    Update VariablesHenry.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariablesHenry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/VariablesHenry.java
@@ -1,12 +1,24 @@
 package org.firstinspires.ftc.teamcode;
 
+import com.acmerobotics.dashboard.FtcDashboard;
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @TeleOp()
 public class VariablesHenry extends OpMode {
     @Override
-    public void init(){ telemetry.addData("Hello","World");}
+    public void init(){
+        FtcDashboard dashboard = FtcDashboard.getInstance();
+        telemetry = new MultipleTelemetry(telemetry, dashboard.getTelemetry());
+        int teamNumber = 16072;
+    double motorSpeed = 0.5;
+    boolean touchSensorPressed = true;
+
+    telemetry.addData("Team Number", teamNumber);
+    telemetry.addData("Motor Speed", motorSpeed);
+    telemetry.addData("Touch Sensor", touchSensorPressed);
+    }
     @Override
     public void loop() {
     }

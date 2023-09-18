30191a837d6140bfb25db1f07cd52dac3cd923ef
Astricklin3 <104530954+henryhull@users.noreply.github.com>
2023-09-18 11:04:24 -0500

    .

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Exercises_4_6_Henry.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Exercises_4_6_Henry.java
@@ -1,33 +1,41 @@
 package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
 
 @TeleOp
 public class Exercises_4_6_Henry extends OpMode {
+    private DcMotor Motor1;
+
     @Override
     public void init() {
+        telemetry.addData("Left Stick x", gamepad1.left_stick_x);
+        telemetry.addData("Left stick y", gamepad1.left_stick_y);
+        Motor1  = hardwareMap.get(DcMotor.class, "Motor1");
     }
 
-        @Override
-        public void loop(){
+    @Override
+    public void loop() {
 
-            telemetry.addData("Left Stick x", gamepad1.left_stick_x);
-            telemetry.addData("Left stick y", gamepad1.left_stick_y);
+        double fwdSpeed = gamepad1.left_stick_y;
+        double ySpeed = gamepad1.left_stick_y;
+        double xSpeed = gamepad1.left_stick_x;
 
-            double fwdSpeed = gamepad1.left_stick_y;
-            double ySpeed = gamepad1.left_stick_y;
-            double xSpeed = gamepad1.left_stick_x;
 
-            if (gamepad1.a) { //turbo
-                fwdSpeed *= 0.5;
-            }
-            if (!gamepad1.a) { //turbo
-                fwdSpeed *= 1;
-            }
+        if (gamepad1.a) { //turbo
+            fwdSpeed *= 0.5;
+        }
+        if (!gamepad1.a) { //turbo
+            fwdSpeed *= 1;
+        }
 
-            if (!gamepad1.a) { //crazy mode
-                ySpeed = gamepad1.left_stick_x;
-                xSpeed = gamepad1.left_stick_y;
-            }
+        if (!gamepad1.a) { //crazy mode
+            ySpeed = gamepad1.left_stick_x;
+            xSpeed = gamepad1.left_stick_y;
+        }
+
+        if (gamepad1.b) {//run motor 1
+            Motor1.setPower(1);
         }
     }
+}

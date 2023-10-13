0a7fd48f6688dcf49a340cbd08f8f2f517027318
Astricklin3 <104530954+henryhull@users.noreply.github.com>
2023-10-13 10:22:00 -0500

    Update Lana_Lifting_Test.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lana_Lifting_Test.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Lana_Lifting_Test.java
@@ -33,7 +33,7 @@ import com.acmerobotics.dashboard.FtcDashboard;
 import com.acmerobotics.dashboard.config.Config;
 import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.qualcomm.robotcore.eventloop.opmode.Disabled;
-import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;,
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorEx;
